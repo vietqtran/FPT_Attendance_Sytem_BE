e4d46b728b32ac82f4c1328de381568502a555dc
Nguyễn Minh Mạnh <103049306+manhminh@users.noreply.github.com>
2024-01-15 23:24:34 +0700

    update code

--- src/main/java/com/fas/services/CampusService.java
+++ src/main/java/com/fas/services/CampusService.java
@@ -5,18 +5,10 @@ import com.fas.models.enums.CampusName;
 
 import java.util.List;
 
-
-<<<<<<< HEAD
-    List<Campus> findAllCampuses();
-
-    public Campus findByCampusId(Long campusId);
-=======
-
 public interface CampusService {
     public Campus findByCampusName(CampusName name);
 
     public Campus findByCampusId(Long campusId);
 
     public List<Campus> findAllCampuses();
->>>>>>> 5706112d3f6a53b2e3d68aed3f81d32fede050fa
 }
